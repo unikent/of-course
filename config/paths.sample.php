53b571bca496636b88c3f7f05d794cf7d778f80d
conatus <alex@recordsonribs.com>
2012-12-10 15:01:37 +0000

    Make Generic

--- config/paths.sample.php
+++ config/paths.sample.php
@@ -4,4 +4,4 @@ const XCRI_WEBSERVICE = ''; //URL of Programmesplant webservice
 
 const BASE_URL= ''; //URL of this system
 
-const PANTHEON_ENGINE= ''; //URL of templating engine
+const TEMPLATING_ENGINE= ''; // URL of templating engine
