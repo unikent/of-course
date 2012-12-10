eecbe9961b863e99974836539250df240f32e586
conatus <alex@recordsonribs.com>
2012-12-10 15:01:49 +0000

    Neaten Comments

--- config/paths.sample.php
+++ config/paths.sample.php
@@ -1,7 +1,7 @@
 <?php
 
-const XCRI_WEBSERVICE = ''; //URL of Programmesplant webservice
+const XCRI_WEBSERVICE = ''; // URL of Programmes Plant API
 
-const BASE_URL= ''; //URL of this system
+const BASE_URL= ''; // URL of this system
 
 const TEMPLATING_ENGINE= ''; // URL of templating engine
