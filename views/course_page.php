aae8eba65a83d13244a9403f018e2663ad11b037
thybag <thybag@gmail.com>
2012-12-14 16:08:07 +0000

    Tweak some styles, remove tab containers from new UI version.

--- views/course_page.php
+++ views/course_page.php
@@ -2,7 +2,7 @@
                 <kentPageBodyFull>
 
                     <kentPageContent>                   
-                    <div class="maps" id="maps">
+                    <div class="courses">
                     
                         <div class="container_16">
                         
@@ -28,57 +28,44 @@
             	
                     	<div class="container_16">
                             <div class="grid_10">
-                        <h1><?php echo  $course->programme_title; ?> <?php echo $course->award->name; ?> - <?php echo $course->year; ?></h1>
-                        <p><?php echo  $course->programme_abstract; ?></p>
-                        <div class="snippetBox">
-                            <div class="tabs">
-                                <ul class="tabsFallBackNav">
-<!--
-                                    <li><a href="#tab1">Overview</a></li>
-                                    <li><a href="#tab2">Structure</a></li>
-                                    <li><a href="#tab3">Teaching and assessment</a></li>
-                                    <li><a href="#tab3">Careers</a></li>
-                                    <li><a href="#tab3">Entry requirements</a></li>
-                                    <li><a href="#tab3">Fees and funding</a></li>
-                                    <li><a href="#tab3">Apply</a></li>
-                                    <li><a href="#tab3">Further info</a></li>
--->
-                                </ul>
+                                 <h1><?php echo  $course->programme_title; ?> <?php echo $course->award->name; ?> - <?php echo $course->year; ?></h1>
                                 <?php Flight::render('tabs/overview2', array('course'=>$course)); ?>
-                                <?php Flight::render('tabs/structure', array('course'=>$course)); ?>
-                                <?php Flight::render('tabs/teaching', array('course'=>$course)); ?>
-                                <?php Flight::render('tabs/careers', array('course'=>$course)); ?>
-                                <?php Flight::render('tabs/entry', array('course'=>$course)); ?>
-                                <?php Flight::render('tabs/fees', array('course'=>$course)); ?>
-                                <?php Flight::render('tabs/apply', array('course'=>$course)); ?>
-                                <?php Flight::render('tabs/info', array('course'=>$course)); ?>
                             </div>
-                        </div>
-                        
-                        
-                    </div><!--/grid_10-->
-                            
                             <div class="grid_6">
                             
-                            <div id="key-facts">
-                                <h2>Key facts</h2>
-                                <ul>
-                                  <li><strong>Subject area:</strong> <?php echo $course->subject_area_1->{1}->name;?></li>
-                                  <li><strong>Award:</strong> <?php echo $course->award->{1}->name;?> </li>
-                                  <li><strong>Honours type:</strong> <?php echo $course->honours_type;?> </li>
-                                  <li><strong>Ucas code:</strong> <?php echo $course->ucas_code;?>  </li>
-                                  <li><strong>Location:</strong> <?php echo $course->location->{1}->name;?>  </li>
-                                  <li><strong>Mode of study:</strong> <?php echo $course->mode_of_study;?> 
-                                  </li>
-                                  <li><strong>Duration:</strong> <?php echo $course->duration;?></li>
-                                  <li><strong>Start: </strong> <?php echo $course->start;?> </li>
-                                  <li><strong>Accredited by</strong>: <?php echo $course->accredited_by;?>  </li>
-                                  <li><strong>Total Kent credits:</strong> <?php echo $course->total_kent_credits_awarded_on_completion;?></li>
-                                  <li><strong>Total ECTS credits:</strong> <?php echo $course->total_ects_credits_awarded_on_completion;?></li>
-                                </ul>
-                            </div>
+                                <div id="key-facts">
+                                    <h2>Key facts</h2>
+                                    <ul>
+                                      <li><strong>Subject area:</strong> <?php echo $course->subject_area_1->{1}->name;?></li>
+                                      <li><strong>Award:</strong> <?php echo $course->award->{1}->name;?> </li>
+                                      <li><strong>Honours type:</strong> <?php echo $course->honours_type;?> </li>
+                                      <li><strong>Ucas code:</strong> <?php echo $course->ucas_code;?>  </li>
+                                      <li><strong>Location:</strong> <?php echo $course->location->{1}->name;?>  </li>
+                                      <li><strong>Mode of study:</strong> <?php echo $course->mode_of_study;?> 
+                                      </li>
+                                      <li><strong>Duration:</strong> <?php echo $course->duration;?></li>
+                                      <li><strong>Start: </strong> <?php echo $course->start;?> </li>
+                                      <li><strong>Accredited by</strong>: <?php echo $course->accredited_by;?>  </li>
+                                      <li><strong>Total Kent credits:</strong> <?php echo $course->total_kent_credits_awarded_on_completion;?></li>
+                                      <li><strong>Total ECTS credits:</strong> <?php echo $course->total_ects_credits_awarded_on_completion;?></li>
+                                    </ul>
+                                </div>
+                             </div>
+
+
+                        <div class="grid_16">
+                        <?php Flight::render('tabs/structure', array('course'=>$course)); ?>
+                        <?php Flight::render('tabs/teaching', array('course'=>$course)); ?>
+                        <?php Flight::render('tabs/careers', array('course'=>$course)); ?>
+                        <?php Flight::render('tabs/entry', array('course'=>$course)); ?>
+                        <?php Flight::render('tabs/fees', array('course'=>$course)); ?>
+                        <?php Flight::render('tabs/apply', array('course'=>$course)); ?>
+                        <?php Flight::render('tabs/info', array('course'=>$course)); ?>
+                        </div>
                         
-                    </div>
+                   
+                            
+                     
                 	</div>
                     </div>
 
