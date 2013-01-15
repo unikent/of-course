46d73823038b093c57f8cde17b125deff7cedf3c
thybag <thybag@gmail.com>
2013-01-15 16:31:33 +0000

    Add search bar mockup to page

--- views/layout.php
+++ views/layout.php
@@ -26,12 +26,13 @@
             quickspot.attach({
                 "url":"<?php echo BASE_URL ?>searchajax/<?php echo $type ?>/<?php echo $year ?>/",
                 "target":"searchbox",
-                "clickhandler":function(itm){
+                "search_on": ["name","award", "subject", "main_school", "ucas_code"],
+                "click_handler":function(itm){
                     //Send em to page
                     document.location = '<?php echo BASE_URL.$type ?>/<?php echo $year ?>/'+itm.id+'/'+itm.slug;
                 },
-                "displayhandler": function(itm){
-                    return itm.name+' (ID: '+itm.id+')'; //Do somthing useful like showing award once we have it.
+                "display_handler": function(itm){
+                    return itm.name+' - <i>'+itm.award+'</i>'; //Do somthing useful like showing award once we have it.
                 }
             });
         </script>
@@ -43,7 +44,111 @@
                 <kentGlobalHeader/>
                 <kentDepartmentHeader hideBanner='true'/>
             </kentPageHeader>
-
+             <div class="search_bar">
+                            <div class="search_box">
+                                <input id='searchbox' type="text" name="search" placeholder="Search all courses..." autocomplete="off">
+                                <input type="image" src="<?php echo BASE_URL ?>/images/search.jpg">
+                            </div>
+                            
+                            <ul class="advsearch_links">
+                                <li class="first advsearchlink"><a href="<?php echo BASE_URL.$type ?>/<?php echo $year ?>/search">Advanced search</a></li>
+                            </ul>
+                                    
+                            <ul class="search_links">
+                                <li class="first toplink"><a href="" id="campuses_and_centres_anchor">Subjects</a>
+                        
+                                    <div id="campuses_and_centres_links" class="megamenu" style="display: none; ">
+                                        <div class="maps_megacontentarea">
+                                            <div class="megacontentsection">
+                                                Architecture
+                                            </div>
+                                            <div class="megacontentsection">
+                                                Finance
+                                            </div>
+                                            <div class="megacontentsection">
+                                                American Studies
+                                            </div>
+                                            <div class="megacontentsection">
+                                                English
+                                            </div>
+                                            <div class="megacontentsection">
+                                                History
+                                            </div>
+                                            <div class="megacontentsection">
+                                                Biology
+                                            </div>
+                                        </div>
+                                    </div>
+                
+                                </li>
+                                
+                                <li class="first toplink"><a href="" id="campuses_and_centres_anchor">Schools</a>
+                        
+                                    <div id="campuses_and_centres_links" class="megamenu" style="display: none; ">
+                                    <div class="maps_megacontentarea">
+                                        <div class="megacontentsection">
+                                            Architecture</h3>
+                                        </div>
+                                        <div class="megacontentsection">
+                                            Finance
+                                        </div>
+                                        <div class="megacontentsection">
+                                            Law
+                                        </div>
+                                        <div class="megacontentsection">
+                                            Anthropology
+                                        </div>
+                                        <div class="megacontentsection">
+                                            Biosciences
+                                        </div>
+                                    </div>
+                                </div>
+                
+                                </li>
+                                
+                                    
+                                <li class="last toplink"><a href="">More</a>
+                                
+                                    <div id="more_links" class="megamenu" style="display: none; ">
+                                        <div class="maps_megacontentarea">
+                                            
+                                            <div class="megacontentsection">
+                                                Undergraduate study
+                                            </div>
+                                            <div class="megacontentsection">
+                                                About
+                                            </div>
+                                            <div class="megacontentsection">
+                                                Courses (2013 entry)
+                                            </div>
+                                            <div class="megacontentsection">
+                                                Applications
+                                            </div>
+                                            <div class="megacontentsection">
+                                                How it works
+                                            </div>
+                                            <div class="megacontentsection">
+                                                Part-time study
+                                            </div>
+                                            <div class="megacontentsection">
+                                                Mature students
+                                            </div>
+                                            <div class="megacontentsection">
+                                                Why study at Kent
+                                            </div>
+                                            <div class="megacontentsection">
+                                                What do  our students say?
+                                            </div>
+                                            <div class="megacontentsection">
+                                                Downloads
+                                            </div>
+                                        </div>
+                                    </div>
+                            
+                                </li>
+                            </ul>
+            
+                        </div>
 
                         <?php echo $content; ?>
                     
@@ -55,4 +160,4 @@
         </kentPage>
     </body>
 <!-- InstanceEnd -->
-</html
+</html>
