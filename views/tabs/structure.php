6b68e8615efaf98bbabcde3c2ae7bc5777e77d6e
Matthew Bull <bull.matthew@gmail.com>
2013-01-22 23:40:59 +0000

    Added display of module data to the structure tab
    This was working earlier, but has now been adapted for the new api

--- views/tabs/structure.php
+++ views/tabs/structure.php
@@ -1,4 +1,3 @@
-
 <div class="tabContent" id="tab2">
   <h2>Course structure</h2>
     <div class="tabs"> 
@@ -16,28 +15,32 @@
           <h3>Stage 1</h3>
           <h4>Compulsory modules</h4>
           <ul>
-              <?php foreach ($course->modules->compulsory_modules->module as $module): ?>
-              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)</li>
+              <?php foreach ($course->modules->response->rubric->compulsory_modules->module as $module): ?>
+              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)
+              <div><?php echo $module->synopsis ?></div>
+              </li>
               <?php endforeach; ?>
           </ul>
           
           <h4>Optional modules</h4>
-          <?php if ( $course->modules->optional_modules->{'-maximum_required'} > $course->modules->optional_modules->{'-required'} ): ?>
-              <p>You must take between <?php echo $course->modules->optional_modules->{'-required'} ?> and <?php echo $course->modules->optional_modules->{'-maximum_required'} ?> credits from the following modules.</p>
+          <?php if ( $course->modules->response->rubric->optional_modules->{'-maximum_required'} > $course->modules->response->rubric->optional_modules->{'-required'} ): ?>
+              <p>You must take between <?php echo $course->modules->response->rubric->optional_modules->{'-required'} ?> and <?php echo $course->modules->response->rubric->optional_modules->{'-maximum_required'} ?> credits from the following modules.</p>
           <?php else: ?>
-              <p>You must take a total of <?php echo $course->modules->optional_modules->{'-required'} ?> credits from the following modules.</p>
+              <p>You must take a total of <?php echo $course->modules->response->rubric->optional_modules->{'-required'} ?> credits from the following modules.</p>
           <?php endif; ?>
           <ul>
-              <?php foreach ($course->modules->optional_modules->module as $module): ?>
-              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)</li>
+              <?php foreach ($course->modules->response->rubric->optional_modules->module as $module): ?>
+              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)
+              <div><?php echo $module->synopsis ?></div>
+              </li>
               <?php endforeach; ?>
           </ul>
           
           <h4>Wildcard modules</h4>
-                    <?php if ( $course->modules->wildcard->{'-maximum_required'} > $course->modules->wildcard->{'-required'} ): ?>
-              <p>You must take between <?php echo $course->modules->wildcard->{'-required'} ?> and <?php echo $course->modules->wildcard->{'-maximum_required'} ?> credits from wildcard modules.</p>
+                    <?php if ( $course->modules->response->rubric->wildcard->{'-maximum_required'} > $course->modules->response->rubric->wildcard->{'-required'} ): ?>
+              <p>You must take between <?php echo $course->modules->response->rubric->wildcard->{'-required'} ?> and <?php echo $course->modules->response->rubric->wildcard->{'-maximum_required'} ?> credits from wildcard modules.</p>
           <?php else: ?>
-              <p>You must take a total of <?php echo $course->modules->wildcard->{'-required'} ?> credits from wildcard modules.</p>
+              <p>You must take a total of <?php echo $course->modules->response->rubric->wildcard->{'-required'} ?> credits from wildcard modules.</p>
           <?php endif; ?>
           
           <h5>Disclaimer</h5>
@@ -49,28 +52,32 @@
           <h3>Stage 2</h3>
           <h4>Compulsory modules</h4>
           <ul>
-              <?php foreach ($course->modules->compulsory_modules->module as $module): ?>
-              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)</li>
+              <?php foreach ($course->modules->response->rubric->compulsory_modules->module as $module): ?>
+              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)
+              <div><?php echo $module->synopsis ?></div>
+              </li>
               <?php endforeach; ?>
           </ul>
           
           <h4>Optional modules</h4>
-          <?php if ( $course->modules->optional_modules->{'-maximum_required'} > $course->modules->optional_modules->{'-required'} ): ?>
-              <p>You must take between <?php echo $course->modules->optional_modules->{'-required'} ?> and <?php echo $course->modules->optional_modules->{'-maximum_required'} ?> credits from the following modules.</p>
+          <?php if ( $course->modules->response->rubric->optional_modules->{'-maximum_required'} > $course->modules->response->rubric->optional_modules->{'-required'} ): ?>
+              <p>You must take between <?php echo $course->modules->response->rubric->optional_modules->{'-required'} ?> and <?php echo $course->modules->response->rubric->optional_modules->{'-maximum_required'} ?> credits from the following modules.</p>
           <?php else: ?>
-              <p>You must take a total of <?php echo $course->modules->optional_modules->{'-required'} ?> credits from the following modules.</p>
+              <p>You must take a total of <?php echo $course->modules->response->rubric->optional_modules->{'-required'} ?> credits from the following modules.</p>
           <?php endif; ?>
           <ul>
-              <?php foreach ($course->modules->optional_modules->module as $module): ?>
-              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)</li>
+              <?php foreach ($course->modules->response->rubric->optional_modules->module as $module): ?>
+              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)
+              <div><?php echo $module->synopsis ?></div>
+              </li>
               <?php endforeach; ?>
           </ul>
           
           <h4>Wildcard modules</h4>
-                    <?php if ( $course->modules->wildcard->{'-maximum_required'} > $course->modules->wildcard->{'-required'} ): ?>
-              <p>You must take between <?php echo $course->modules->wildcard->{'-required'} ?> and <?php echo $course->modules->wildcard->{'-maximum_required'} ?> credits from wildcard modules.</p>
+                    <?php if ( $course->modules->response->rubric->wildcard->{'-maximum_required'} > $course->modules->response->rubric->wildcard->{'-required'} ): ?>
+              <p>You must take between <?php echo $course->modules->response->rubric->wildcard->{'-required'} ?> and <?php echo $course->modules->response->rubric->wildcard->{'-maximum_required'} ?> credits from wildcard modules.</p>
           <?php else: ?>
-              <p>You must take a total of <?php echo $course->modules->wildcard->{'-required'} ?> credits from wildcard modules.</p>
+              <p>You must take a total of <?php echo $course->modules->response->rubric->wildcard->{'-required'} ?> credits from wildcard modules.</p>
           <?php endif; ?>
           
           <h5>Disclaimer</h5>
@@ -81,28 +88,32 @@
           <h3>Stage 3</h3>
           <h4>Compulsory modules</h4>
           <ul>
-              <?php foreach ($course->modules->compulsory_modules->module as $module): ?>
-              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)</li>
+              <?php foreach ($course->modules->response->rubric->compulsory_modules->module as $module): ?>
+              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)
+              <div><?php echo $module->synopsis ?></div>
+              </li>
               <?php endforeach; ?>
           </ul>
           
           <h4>Optional modules</h4>
-          <?php if ( $course->modules->optional_modules->{'-maximum_required'} > $course->modules->optional_modules->{'-required'} ): ?>
-              <p>You must take between <?php echo $course->modules->optional_modules->{'-required'} ?> and <?php echo $course->modules->optional_modules->{'-maximum_required'} ?> credits from the following modules.</p>
+          <?php if ( $course->modules->response->rubric->optional_modules->{'-maximum_required'} > $course->modules->response->rubric->optional_modules->{'-required'} ): ?>
+              <p>You must take between <?php echo $course->modules->response->rubric->optional_modules->{'-required'} ?> and <?php echo $course->modules->response->rubric->optional_modules->{'-maximum_required'} ?> credits from the following modules.</p>
           <?php else: ?>
-              <p>You must take a total of <?php echo $course->modules->optional_modules->{'-required'} ?> credits from the following modules.</p>
+              <p>You must take a total of <?php echo $course->modules->response->rubric->optional_modules->{'-required'} ?> credits from the following modules.</p>
           <?php endif; ?>
           <ul>
-              <?php foreach ($course->modules->optional_modules->module as $module): ?>
-              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)</li>
+              <?php foreach ($course->modules->response->rubric->optional_modules->module as $module): ?>
+              <li><a href="http://www.kent.ac.uk/courses/modulecatalogue/modules/<?php echo $module->{'-code'} ?>"><?php echo $module->{'-title'} ?></a> (<?php echo $module->{'-credits'} ?> credits)
+              <div><?php echo $module->synopsis ?></div>
+              </li>
               <?php endforeach; ?>
           </ul>
           
           <h4>Wildcard modules</h4>
-                    <?php if ( $course->modules->wildcard->{'-maximum_required'} > $course->modules->wildcard->{'-required'} ): ?>
-              <p>You must take between <?php echo $course->modules->wildcard->{'-required'} ?> and <?php echo $course->modules->wildcard->{'-maximum_required'} ?> credits from wildcard modules.</p>
+                    <?php if ( $course->modules->response->rubric->wildcard->{'-maximum_required'} > $course->modules->response->rubric->wildcard->{'-required'} ): ?>
+              <p>You must take between <?php echo $course->modules->response->rubric->wildcard->{'-required'} ?> and <?php echo $course->modules->response->rubric->wildcard->{'-maximum_required'} ?> credits from wildcard modules.</p>
           <?php else: ?>
-              <p>You must take a total of <?php echo $course->modules->wildcard->{'-required'} ?> credits from wildcard modules.</p>
+              <p>You must take a total of <?php echo $course->modules->response->rubric->wildcard->{'-required'} ?> credits from wildcard modules.</p>
           <?php endif; ?>
           
           <h5>Disclaimer</h5>
