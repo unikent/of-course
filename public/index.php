f29d5fd0e3d5bf3db7c310981a00f1a42f8e3541
Matthew Bull <bull.matthew@gmail.com>
2013-02-08 15:43:20 +0000

    Modified routing to put more specific routes before less specific ones

--- public/index.php
+++ public/index.php
@@ -32,22 +32,21 @@ $main = new CoursesFrontEnd();
 // Define routes
 
 // AJAX
-Flight::route('/ajax/subjects/', array($main,'ajax_subjects_page'));
+Flight::route('/ajax/subjects/@type/@year', array($main,'ajax_subjects_page'));
 Flight::route('/ajax/search/@type/@year/', array($main,'ajax_search_data'));
 // Preview
 Flight::route('/preview/@hash', array($main,'preview'));
 // key pages
+Flight::route('/@type/@year/search/@search_type/@search_string', array($main,'search'));
 Flight::route('/@type/@year/search', array($main,'search'));
 Flight::route('/@type/@year/', array($main,'list_programmes'));
 // Subjects
-Flight::route('/@type/@year/subjects', array($main,'subjects'));
 Flight::route('/@type/@year/subjects/@id/@slug', array($main,'subject_view'));
+Flight::route('/@type/@year/subjects', array($main,'subjects'));
 // courses
-Flight::route('/@type/@year/@id', array($main,'view'));
 Flight::route('/@type/@year/@id/@slug', array($main,'view'));
+Flight::route('/@type/@year/@id', array($main,'view'));
 
 
 // Run Flight!
 Flight::start();
-
-?>
