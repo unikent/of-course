a543a9d1f3afe2233857b5f1e34fcfe46cec5e70
conatus <alex@recordsonribs.com>
2013-02-11 14:55:44 +0000

    Yet More Description Of How To Set This Up

--- public/config/paths.sample.php
+++ public/config/paths.sample.php
@@ -4,13 +4,13 @@
  */
 
 // Paths
-const APP_PATH = '';            // Path to the root of the of-course app
-const VENDOR_PATH = '';         // Path to vendor folder.
-const TEMPLATING_ENGINE = ''; 	// Path of templating engine.
+const APP_PATH = '';            // Path to the root of the Of Course application.
+const VENDOR_PATH = '';         // Path to vendor folder - likely APP_PATH/vendor/.
+const TEMPLATING_ENGINE = ''; 	// Path of templating engine, for example Pantheon.
 
 // URLs
-const BASE_URL = '';            // URL of this system
-const XCRI_WEBSERVICE = '';     // URL of Programmes Plant API
+const BASE_URL = '';            // URL of this system - e.g. http://of-course.dev/.
+const XCRI_WEBSERVICE = '';     // URL of Programmes Plant API - e.g. http://pp.dev/api/.
 
 // Caches
 const CACHE_DIRECTORY = '';     // Directory to store caches.
