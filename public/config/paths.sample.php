5e215892e011fd74aa35af01f3c5800abac945bc
conatus <alex@recordsonribs.com>
2013-02-11 14:43:27 +0000

    Improve Clarity Of Sample Configuration

--- public/config/paths.sample.php
+++ public/config/paths.sample.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * Setup Of Course.
+ */
 
 // Paths
-const APP_PATH = ''; 	// path to the root of the of-course app
-const VENDOR_PATH = ''; // Path to vendor folder
-const BASE_URL= ''; 	// URL of this system
+const APP_PATH = '';            // Path to the root of the of-course app
+const VENDOR_PATH = '';         // Path to vendor folder
+const TEMPLATING_ENGINE = ''; 	// URL of templating engine
 
-const XCRI_WEBSERVICE = ''; 	// URL of Programmes Plant API
-const TEMPLATING_ENGINE= ''; 	// URL of templating engine
+// URLs
+const BASE_URL = '';            // URL of this system
+const XCRI_WEBSERVICE = '';     // URL of Programmes Plant API
 
-const CACHE_DIRECTORY = '';
+// Caches
+const CACHE_DIRECTORY = '';     // Directory to store caches.
