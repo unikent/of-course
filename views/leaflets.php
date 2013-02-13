1286dc68bbcdc2545a172930f89a59f58b32732e
jtisqo <jay@tisqo.com>
2013-02-13 09:59:32 +0000

    adding comments and tidying up subject leaflets view

--- views/leaflets.php
+++ views/leaflets.php
@@ -1,37 +1,43 @@
 <?php
   
-  $leaflet_atoz = array();
-  foreach ($leaflets as $leaflet) {
-    $leaflet_atoz[strtoupper(substr($leaflet->name, 0, 1))][] = $leaflet;
-  }
+// our A to Z array of subject leaflets
+$leaflet_atoz = array();
 
-  // create the content of our a to z list
+//oreganise the subject leaflets into and A to Z array
+foreach ($leaflets as $leaflet) {
+  $leaflet_atoz[strtoupper(substr($leaflet->name, 0, 1))][] = $leaflet;
+}
+
+// our tab navigation
 $tabs_nav = "";
+
+// our tab contents
 $tabs_content = "";
-$count = 0;
-foreach(array_keys($leaflet_atoz) as $key){
 
-  $active = ($count == 0) ? 'active' : '';
-    
-    $tabs_nav .= '<li class="' . $active . '"><a href="#'.$key.'" data-toggle="tab">'.$key.'</a></li>';
+// for each letter in our A to Z, build our tabs
+foreach(array_keys($leaflet_atoz) as $key => $letter){
 
-    $tabs_content .= '<section class="tab-pane ' . $active . '" id="'.$key.'"><ul>';
-    foreach ($leaflet_atoz[$key] as $leaflet) {
-      $leaflet_name = $leaflet->name;
-      if(!empty($leaflet->tracking_code)){
-        $leaflet_name = '<a href="'.$leaflet->tracking_code.'">'.$leaflet_name.'</a>';
-      }
+  $active = ($key == 0) ? 'active' : '';
+  
+  // add the letter to our tab navigation
+  $tabs_nav .= '<li class="' . $active . '"><a href="#'.$letter.'" data-toggle="tab">'.$letter.'</a></li>';
 
-      $tabs_content .= '<li>'.$leaflet_name.'</li>';
+  // add the subject leaflets to the content
+  $tabs_content .= '<section class="tab-pane ' . $active . '" id="'.$letter.'"><ul>';
+  foreach ($leaflet_atoz[$letter] as $leaflet) {
+    $leaflet_name = $leaflet->name;
+    if(!empty($leaflet->tracking_code)){
+      $leaflet_name = '<a href="'.$leaflet->tracking_code.'">'.$leaflet_name.'</a>';
     }
-    $tabs_content .= '</ul></section>';
 
-    $count++;
+    $tabs_content .= '<li>'.$leaflet_name.'</li>';
+  }
+  $tabs_content .= '</ul></section>';
   
 }
 ?>
-<article class="container">
-    <h1>Subject leaflets</h1>
+          <article class="container">
+            <h1>Subject leaflets</h1>
               
             <p>These subject leaflets supplement the information contained in our paper   prospectus and the online subject pages. Though all are slightly different to   reflect the different nature of the subjects we offer, they typically   include:</p>
             <ul>
@@ -47,22 +53,22 @@ foreach(array_keys($leaflet_atoz) as $key){
             <h1>Download PDFs </h1>
             <p>Listed alphabetically by subject</p>
 
-          <div class="row-fluid">
-            <div class="span12">
-              <ul class="nav nav-tabs">
-              <?php echo $tabs_nav ?>
-              </ul>
-            </div><!-- /span -->
-          </div><!-- /row -->
+            <div class="row-fluid">
+              <div class="span12">
+                <ul class="nav nav-tabs">
+                <?php echo $tabs_nav ?>
+                </ul>
+              </div><!-- /span -->
+            </div><!-- /row -->
 
-          <div class="row-fluid">
-            <div class="span12">
-              <div class="tab-content">
-                <?php echo $tabs_content ?>
-              </div>
-            </div><!-- /span -->
-              
-          </div>
-          <p>&nbsp;</p>
-          <p>&nbsp;</p>
-</article>
+            <div class="row-fluid">
+              <div class="span12">
+                <div class="tab-content">
+                  <?php echo $tabs_content ?>
+                </div>
+              </div><!-- /span -->
+                
+            </div>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+          </article>
