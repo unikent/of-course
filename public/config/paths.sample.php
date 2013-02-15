26b357fc49490e910b5fa7c6cf74f9f76990cc17
thybag <thybag@gmail.com>
2013-02-15 14:49:54 +0000

    Add asset url to sample config

--- public/config/paths.sample.php
+++ public/config/paths.sample.php
@@ -5,11 +5,14 @@
 
 // Paths
 const APP_PATH = '';            // Path to the root of the Of Course application.
+
+
 const VENDOR_PATH = '';         // Path to vendor folder - likely APP_PATH/vendor/.
 const TEMPLATING_ENGINE = ''; 	// Path of templating engine, for example Pantheon.
 
 // URLs
-const BASE_URL = '';            // URL of this system - e.g. http://of-course.dev/.
+const BASE_URL = '';            // URL of this system - e.g. / or /courses
+const ASSET_URL= '';			// URL of system assets - e.g /assets or /courses/assets
 const XCRI_WEBSERVICE = '';     // URL of Programmes Plant API - e.g. http://pp.dev/api/.
 
 // Caches
