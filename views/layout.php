b670e8c1ce168bf712289c52ff944242d1a16e8d
thybag <thybag@gmail.com>
2013-02-15 14:48:28 +0000

    Correct CSS asset

--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,7 @@
 	<kentMeta>
 		<!-- InstanceBeginEditable name="metadata" -->
 		<title></title>
-		<link href="<?php echo BASE_URL; ?>/css/import.css" type="text/css" rel="stylesheet">
+		<link href="<?php echo ASSET_URL; ?>/css/import.css" type="text/css" rel="stylesheet">
 		<!-- InstanceEndEditable -->
 	</kentMeta>
 	<kentContent>
