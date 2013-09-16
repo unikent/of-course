6a7e8593a55d2a7015518379c28d3b3d12e50fd5
Mark <=>
2013-09-16 19:18:38 +0100

    Slight update to text

--- views/ug_tabs/apply.php
+++ views/ug_tabs/apply.php
@@ -40,7 +40,7 @@
 			?>
 			<a href="<?php echo $apply ?>" class="apply-link" <?php echo $event_track ?>>Apply for <strong><?php echo $course->programme_title; ?> <?php echo $course->award[0]->name; ?></strong> - <span class="apply-type-link">Part time</span></a>
 		<?php else:?>
-			<p class="apply-link"><strong><?php echo $course->programme_title; ?> <?php echo $course->award[0]->name; ?></strong> - <span class="apply-type-link">Part time</span><br /><br />This course is not currently open for applications. If you would like to be informed when we are accepting applications, please email <a href="mailto:information@kent.ac.uk">information@kent.ac.uk</a>.</p>
+			<p class="apply-link"><strong><?php echo $course->programme_title; ?> <?php echo $course->award[0]->name; ?></strong> - <span class="apply-type-link">Part time</span><br /><br />This part-time course is not currently open for applications. If you would like to be informed when we are accepting applications, please email <a href="mailto:information@kent.ac.uk">information@kent.ac.uk</a>.</p>
 			
 		<?php endif;?>
 	</form>
