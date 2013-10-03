189f27e0ddd430e87a1e11d0088062ba89f4d62b
Matthew Bull <m.bull@kent.ac.uk>
2013-10-03 15:33:47 +0100

    fixed wrong sizing and ordering in subject categories listing

--- views/menus/subjects.php
+++ views/menus/subjects.php
@@ -1,35 +1,39 @@
 <?php
 // Divide our array in half so we can display it in two columns
-$subjects = (array)$subjects;
-$len = count($subjects);
-$quater =  round($len / 4, 0, PHP_ROUND_HALF_UP);
+$subject_array = array();
+foreach ($subjects as $id=>$subject)
+{
+	$subject_array["$subject->name"] = $id;
+}
 
-$subjects_a = array_slice($subjects, 0, $quater );
-$subjects_b = array_slice($subjects, $quater, $quater );
-$subjects_c = array_slice($subjects, $quater*2, $quater);
-$subjects_d = array_slice($subjects, $quater*3, $quater);
+ksort($subject_array);
+$quarter = round( (count($subject_array) / 4), 0, PHP_ROUND_HALF_UP );
+
+$subjects_a = array_slice($subject_array, 0, $quarter);
+$subjects_b = array_slice($subject_array, $quarter, $quarter);
+$subjects_c = array_slice($subject_array, $quarter*2, $quarter);
+$subjects_d = array_slice($subject_array, $quarter*3);
 
 $year_for_url = empty($year) ? '' : ((strcmp($year, CoursesFrontEnd::$current_year) == 0) ? '' : $year . '/');
 
 ?>
 <div class='span3'>
-	<?php foreach($subjects_a as $subject): ?>
-
-	  <a href='<?php echo Flight::url("{$level}/{$year_for_url}search/subject_category/" . urlencode($subject->name)); ?>'><?php echo $subject->name; ?></a>
+	<?php foreach($subjects_a as $subject=>$id): ?>
+	  <a href='<?php echo Flight::url("{$level}/{$year_for_url}search/subject_category/" . urlencode($subject)); ?>'><?php echo $subject; ?></a>
 	<?php endforeach;?>
 </div>
 <div class='span3'>
-	<?php foreach($subjects_b as $subject): ?>
-	  <a href='<?php echo Flight::url("{$level}/{$year_for_url}search/subject_category/" . urlencode($subject->name)); ?>'><?php echo $subject->name; ?></a>
+	<?php foreach($subjects_b as $subject=>$id): ?>
+	  <a href='<?php echo Flight::url("{$level}/{$year_for_url}search/subject_category/" . urlencode($subject)); ?>'><?php echo $subject; ?></a>
 	<?php endforeach;?>
 </div>
 <div class='span3'>
-	<?php foreach($subjects_c as $subject): ?>
-	  <a href='<?php echo Flight::url("{$level}/{$year_for_url}search/subject_category/" . urlencode($subject->name)); ?>'><?php echo $subject->name; ?></a>
+	<?php foreach($subjects_c as $subject=>$id): ?>
+	  <a href='<?php echo Flight::url("{$level}/{$year_for_url}search/subject_category/" . urlencode($subject)); ?>'><?php echo $subject; ?></a>
 	<?php endforeach;?>
 </div>
 <div class='span3'>
-	<?php foreach($subjects_d as $subject): ?>
-	  <a href='<?php echo Flight::url("{$level}/{$year_for_url}search/subject_category/" . urlencode($subject->name)); ?>'><?php echo $subject->name; ?></a>
+	<?php foreach($subjects_d as $subject=>$id): ?>
+	  <a href='<?php echo Flight::url("{$level}/{$year_for_url}search/subject_category/" . urlencode($subject)); ?>'><?php echo $subject; ?></a>
 	<?php endforeach;?>
 </div>
