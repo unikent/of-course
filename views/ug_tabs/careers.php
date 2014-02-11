bf787dd7466c203256feb365adcb380ca3d4f499
Matthew Bull <m.bull@kent.ac.uk>
2014-02-11 12:05:26 +0000

    renamed 'did you know...' text to 'independent rankings' and moved to overview tab
    closes #472

--- views/ug_tabs/careers.php
+++ views/ug_tabs/careers.php
@@ -11,10 +11,3 @@
 	<?php echo $course->professional_recognition; ?>	
 </section>
 <?php endif; ?>
-
-<?php if( ! empty($course->did_you_know_fact_box) ): ?>
-<div class="panel content-highlight">
-	<h4>Did you know...</h4>
-	<?php echo $course->did_you_know_fact_box ?>
-</div>
-<?php endif; ?>
