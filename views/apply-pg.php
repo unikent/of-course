46888a41bd26989ca5a8bdc5d59c1f5b53910683
Matthew Bull <m.bull@kent.ac.uk>
2014-07-17 15:35:01 +0100

    changed link to course, and h1/h2 text

--- views/apply-pg.php
+++ views/apply-pg.php
@@ -3,10 +3,10 @@ $has_parttime = (strpos(strtolower($course->mode_of_study), 'part-time') !== fal
 $has_fulltime = (strpos(strtolower($course->mode_of_study), 'full-time') !== false);
 ?>
 
-<h1>Your application - <a href="/courses/postgraduate/<?php echo $course->instance_id ?>/<?php echo $course->slug ?>"><?php echo $course->programme_title ?></a></h1>
+<h1>Your application</h1>
 
 <div class="apply-form hidden">
-	<h2>Select your course options.</h2>
+	<h2>Select your course options for <a href="/courses/postgraduate/<?php echo $course->instance_id ?>/<?php echo $course->slug ?>"><?php echo $course->programme_title ?></a></h2>
 
 	<p>To begin your application process, you'll need to select your course options below.</p>
 
