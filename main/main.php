64f50039a4e794dbb124b0f18513ea8c49afd523
jtisqo <jay@tisqo.com>
2014-09-11 15:07:06 +0100

    change the apply page logic so that we only go the evision when there is one delivery

--- main/main.php
+++ main/main.php
@@ -713,13 +713,18 @@ class CoursesFrontEnd {
 
  		switch($level){
  			case 'postgraduate':
- 				if ( sizeof($course->award) === 1 && ( trim(strtolower($course->mode_of_study)) == 'part-time only' || trim(strtolower($course->mode_of_study)) == 'full-time only' ) ) {
- 					foreach ($course->deliveries as $delivery) {
-						$url = "https://evision.kent.ac.uk/urd/sits.urd/run/siw_ipp_lgn.login?process=siw_ipp_app&code1=" . $delivery->mcr . "&code2=" . $delivery->current_ipo;
+ 				
+				if (count($course->deliveries) == 1) {
+					$delivery = $course->deliveries[0];
+					$url = (!empty($delivery->current_ipo)) ? "https://evision.kent.ac.uk/urd/sits.urd/run/siw_ipp_lgn.login?process=siw_ipp_app&code1=" . $delivery->mcr . "&code2=" . $delivery->current_ipo : '';
+
+					if (!empty($url)) {
+						header('Location: ' . $url);
+					exit;
 					}
- 					header('Location: ' . $url);
- 					exit;
- 				}
+				}
+ 					
+ 				
 				if($year && ($year !== static::$current_year)){
 					$meta['title'] = "{$course->programme_title} | Postgraduate Programmes {$year} Application | The University of Kent";
 				}
