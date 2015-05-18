265806553b87cb21edc7e342f669f84dfd2ff8b6
eg270-unikent <e.gormley@kent.ac.uk>
2015-05-18 13:54:58 +0100

    add banner logic

--- views/layout.php
+++ views/layout.php
@@ -2,29 +2,29 @@
 <!-- TEMPLATE_VERSION="Daedalus v1.0" -->
 <kentWrapper type="courses" siteroot="<?php echo BASE_URL != '/' ? BASE_URL : ''; ?>/" mode="<?php echo $level;?>" year="<?php echo $year;?>" currentyear="current">
 
-	<kentMeta>
-		<!-- InstanceBeginEditable name="metadata" -->
-		<?php if(isset($meta) && isset($meta['title'])): ?>
-		<title><?php echo $meta['title']; ?></title>
-		<?php endif; ?>
+  <kentMeta>
+    <!-- InstanceBeginEditable name="metadata" -->
+    <?php if(isset($meta) && isset($meta['title'])): ?>
+    <title><?php echo $meta['title']; ?></title>
+    <?php endif; ?>
 
-		<?php if(isset($meta) && isset($meta['description'])): ?>
-		<meta name="description" content="<?php echo $meta['description']; ?>" /> 
-		<?php endif; ?>
+    <?php if(isset($meta) && isset($meta['description'])): ?>
+    <meta name="description" content="<?php echo $meta['description']; ?>" />
+    <?php endif; ?>
 
-		<?php if(isset($meta) && isset($meta['canonical'])): ?>
-		<link rel="canonical" href="<?php echo $meta['canonical']; ?>" />
-		<?php endif; ?>
-		<link rel="feed" type="application/xcri+xml" href="/courses/xcri"/>
+    <?php if(isset($meta) && isset($meta['canonical'])): ?>
+    <link rel="canonical" href="<?php echo $meta['canonical']; ?>" />
+    <?php endif; ?>
+    <link rel="feed" type="application/xcri+xml" href="/courses/xcri"/>
 
-		<link media='screen' type='text/css' rel='stylesheet' href='<?php echo ASSET_URL ?>/css/courses.css' />
-		<link media='screen' type='text/css' rel='stylesheet' href='<?php echo ASSET_URL ?>/css/courses-form.css' />
-		<link media='print' type='text/css' rel='stylesheet' href='<?php echo ASSET_URL ?>/css/print.css' />
-		<!-- InstanceEndEditable -->
-	</kentMeta>
-	<kentContent>
-		<!-- InstanceBeginEditable name="content" -->
-			<kentAttribute name='last_updated' default='<?php echo time(); ?>'><?php echo Flight::last_modified(); ?></kentAttribute>
+    <link media='screen' type='text/css' rel='stylesheet' href='<?php echo ASSET_URL ?>/css/courses.css' />
+    <link media='screen' type='text/css' rel='stylesheet' href='<?php echo ASSET_URL ?>/css/courses-form.css' />
+    <link media='print' type='text/css' rel='stylesheet' href='<?php echo ASSET_URL ?>/css/print.css' />
+    <!-- InstanceEndEditable -->
+  </kentMeta>
+  <kentContent>
+    <!-- InstanceBeginEditable name="content" -->
+      <kentAttribute name='last_updated' default='<?php echo time(); ?>'><?php echo Flight::last_modified(); ?></kentAttribute>
                 <?php if(isset($preview) && $preview == true):?>
                     <meta name="robots" content="noindex, nofollow" />
                     <div class='alert alert-error' style="padding: 10px;margin:10px 0 0 0;">
@@ -40,29 +40,34 @@
                     <?php elseif ( isset($course) && $course->current_year == $course->year ): ?>
                         <meta name="robots" content="noindex, nofollow" />
                         <div class='alert alert-daedalus' style="padding: 20px;margin:10px 0 0 0;">
-                		<?php
-                		$exists = true;
-						$file_headers = get_headers("http://www.kent.ac.uk/courses/undergraduate/" . ($course->current_year - 1) . "/" . $course->instance_id);
-						if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
-    						$exists = false;
-    					}
-						?>
-						<?php if ($exists): ?>
+                    <?php
+                    $exists = true;
+            $file_headers = get_headers("http://www.kent.ac.uk/courses/undergraduate/" . ($course->current_year - 1) . "/" . $course->instance_id);
+            if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+                $exists = false;
+              }
+            ?>
+            <?php if ($exists): ?>
                         <strong>This is a <?php echo $course->current_year;?> entry programme</strong>. If you are a <strong>Clearing</strong>, <strong>Adjustment</strong> or <strong>part-time</strong> applicant wishing to start this September, please view <a href="/courses/undergraduate/<?php echo $course->current_year - 1;?>/<?php echo $course->instance_id ?>/<?php echo $course->slug ?>"><?php echo $course->programme_title;?></a> for <?php echo $course->current_year - 1;?> entry.
-                    
-                		<?php else: ?>
-                		<strong>This is a <?php echo $course->current_year;?> entry programme</strong>. If you are a <strong>Clearing</strong>, <strong>Adjustment</strong> or <strong>part-time</strong> applicant wishing to start this September, please view the <a href="/courses/undergraduate/<?php echo $course->current_year - 1;?>/search/"><?php echo $course->current_year - 1;?> entry online prospectus.</a>
+
+                    <?php else: ?>
+                    <strong>This is a <?php echo $course->current_year;?> entry programme</strong>. If you are a <strong>Clearing</strong>, <strong>Adjustment</strong> or <strong>part-time</strong> applicant wishing to start this September, please view the <a href="/courses/undergraduate/<?php echo $course->current_year - 1;?>/search/"><?php echo $course->current_year - 1;?> entry online prospectus.</a>
                         <?php endif; ?>
                     <?php endif; ?>
-                	</div>
+                  </div>
                 <?php endif;?>
                 <?php if($course->current_year > $course->year): ?>
-                	<meta name="robots" content="noindex, nofollow" />
+                  <meta name="robots" content="noindex, nofollow" />
                     <div class='alert alert-daedalus'>
                         This is a <?php echo $course->year;?> entry programme. Would you like to <a href='<?php echo $meta['active_instance']; ?>'> view <?php echo $course->programme_title;?> for <?php echo $course->current_year;?> entry?</a>
                     </div>
+                <?php elseif(!empty($course->current_year) && $course->current_year === $course->year): ?>
+                  <meta name="robots" content="noindex, nofollow" />
+                  <div id="noAlert">
+                    This is a <?php echo $course->year;?> entry programme. Would you like to <a href='<?php echo $meta['active_instance']; ?>'> view <?php echo $course->programme_title;?> for <?php echo $course->current_year-1;?> entry?</a>
+                  </div>
                 <?php elseif($course->current_year < $course->year): ?>
-                	<meta name="robots" content="noindex, nofollow" />
+                  <meta name="robots" content="noindex, nofollow" />
                     <div class='alert alert-error' style="padding: 10px;margin:10px 0 0 0;">
                         You are currently viewing a programme for an upcoming academic year. This data is preview ONLY and may not be representative of any course offered by this institution.
                     </div>
@@ -72,15 +77,15 @@
 
 
               <a href='#' onclick= "$('html, body').scrollTop();" class='scroll-to-top'>
-             	 <i class="icon-chevron-up icon-white"></i>
+                <i class="icon-chevron-up icon-white"></i>
               </a>
 
-		<!-- InstanceEndEditable -->
-	</kentContent>	
-	<kentScripts>
-		<script type="text/javascript" charset="utf8" src="//ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.1/jquery.dataTables.min.js"></script>
-		<script type="text/javascript" charset="utf-8" language="javascript" src="<?php echo ASSET_URL ?>/js/build/of-course.min.js"></script>
-		<script type="text/javascript" src="/pantheon/static/daedalus/scripts/lib/bootstrap/2.3.2/bootstrap-tooltip.js"></script>
-	</kentScripts>
-</kentWrapper>	
+    <!-- InstanceEndEditable -->
+  </kentContent>
+  <kentScripts>
+    <script type="text/javascript" charset="utf8" src="//ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.1/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" charset="utf-8" language="javascript" src="<?php echo ASSET_URL ?>/js/build/of-course.min.js"></script>
+    <script type="text/javascript" src="/pantheon/static/daedalus/scripts/lib/bootstrap/2.3.2/bootstrap-tooltip.js"></script>
+  </kentScripts>
+</kentWrapper>
 <!-- InstanceEnd -->
