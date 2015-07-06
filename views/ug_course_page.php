f9c9e6b99013424ac0c1be46aca4209ac4b7221a
eg270-unikent <e.gormley@kent.ac.uk>
2015-07-06 12:32:58 +0100

    more clearing changes

--- views/ug_course_page.php
+++ views/ug_course_page.php
@@ -66,8 +66,7 @@ else: ?>
     <div class="span5">
 
         <div class="side-panel">
-
-            <?php if (defined('CLEARING') && CLEARING && $course->current_year > $course->year): ?>
+          <?php if (defined('CLEARING') && CLEARING && $course->current_year > $course->year): ?>
                 <div class="panel admission-links">
                     <a href="<?php echo $course->globals->clearing_vacancies_link; ?>">Is this course in Clearing?</a>
                 </div>
