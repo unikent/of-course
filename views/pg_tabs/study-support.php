4055ba36e5f3a287a9f5cf11c5d75ba19b4ad139
gjmh-unikent <g.haddow@kent.ac.uk>
2015-09-29 16:49:04 +0100

    Add RDP and GSA info to PG course pages

--- views/pg_tabs/study-support.php
+++ views/pg_tabs/study-support.php
@@ -1,3 +1,23 @@
 <h2>Study support</h2>
 
 <?php echo !empty($course->key_information_miscellaneous) ? $course->key_information_miscellaneous : '' ?>
+
+<?php
+    if (strpos($course->programme_type, 'research') === false) { ?>
+        <?php if(!empty($course->globals->global_skills_award)){
+        ?>
+            <h2>Global Skills Award</h2>
+        <?php
+            echo $course->globals->global_skills_award;
+        }
+        ?>
+<?php }elseif (strpos($course->programme_type, 'taught') === false){ ?>
+    <?php
+        if(!empty($course->globals->researcher_development_programme)) {
+        ?>
+            <h2>Researcher Development Programme</h2>
+        <?php
+            echo $course->globals->researcher_development_programme;
+        }
+        ?>
+<?php } ?>
