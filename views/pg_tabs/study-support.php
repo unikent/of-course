66172c52fabad7a7bcd22d28980109df3ff4b15a
gjmh-unikent <g.haddow@kent.ac.uk>
2015-09-30 09:40:22 +0100

    allow suppression of new fields

--- views/pg_tabs/study-support.php
+++ views/pg_tabs/study-support.php
@@ -3,21 +3,24 @@
 <?php echo !empty($course->key_information_miscellaneous) ? $course->key_information_miscellaneous : '' ?>
 
 <?php
+if(!$course->disable_studysupport_additional) {
     if (strpos($course->programme_type, 'research') === false) { ?>
-        <?php if(!empty($course->globals->global_skills_award)){
-        ?>
+        <?php if (!empty($course->globals->global_skills_award)) {
+            ?>
             <h2>Global Skills Award</h2>
-        <?php
+            <?php
             echo $course->globals->global_skills_award;
         }
         ?>
-<?php }elseif (strpos($course->programme_type, 'taught') === false){ ?>
-    <?php
-        if(!empty($course->globals->researcher_development_programme)) {
-        ?>
-            <h2>Researcher Development Programme</h2>
+    <?php } elseif (strpos($course->programme_type, 'taught') === false) { ?>
         <?php
+        if (!empty($course->globals->researcher_development_programme)) {
+            ?>
+            <h2>Researcher Development Programme</h2>
+            <?php
             echo $course->globals->researcher_development_programme;
         }
         ?>
-<?php } ?>
+    <?php }
+}
+?>
