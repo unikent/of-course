8b016e0a19b01aee39de6905b633aeff3f825ed9
thybag <thybag@gmail.com>
2015-10-02 11:31:01 +0100

    fix route mode condition (local is off by default)

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 include "../app/bootstrap.php";
 
 // shim paths (since this sits along side other content, its base path is one level down)
-if(!defined("LOCAL") && !LOCAL){
+if(!defined("LOCAL") || !LOCAL){
 	Flight::request()->base = dirname(Flight::request()->base);
 	Flight::request()->url = substr(Flight::request()->url, strlen(Flight::request()->base));
 }
