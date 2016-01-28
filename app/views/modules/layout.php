6278bf82c4d2f1c153d1574b869b6db9ec8dcaa3
thybag <thybag@gmail.com>
2016-01-28 13:33:10 +0000

    Add routes and rough controller + basic views

---
+++ app/views/modules/layout.php
@@ -0,0 +1,26 @@
+<!-- InstanceBegin template="/Templates/daedalus_v1.dwt" codeOutsideHTMLIsLocked="false" -->
+<!-- TEMPLATE_VERSION="Daedalus v1.0" -->
+<kentWrapper type="simple">
+
+  <kentMeta>
+	<!-- InstanceBeginEditable name="metadata" -->
+	<?php if(isset($meta) && isset($meta['title'])): ?>
+		<title><?php echo $meta['title']; ?></title>
+	<?php endif; ?>
+
+	<?php if(isset($meta) && isset($meta['description'])): ?>
+		<meta name="description" content="<?php echo $meta['description']; ?>" />
+	<?php endif; ?>
+
+	<!-- InstanceEndEditable -->
+  </kentMeta>
+  <kentContent>
+	<!-- InstanceBeginEditable name="content" -->
+	 
+	 <?php echo $content; ?>
+
+	<!-- InstanceEndEditable -->
+  </kentContent>
+
+</kentWrapper>
+<!-- InstanceEnd -->
