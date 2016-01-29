f771e53b2a1b7aaea41070c3c2af938f1fbe0850
eg270-unikent <e.gormley@kent.ac.uk>
2016-01-29 13:10:46 +0000

    map old URLs to new ones

--- app/controllers/ModulesController.php
+++ app/controllers/ModulesController.php
@@ -11,7 +11,7 @@ class ModulesController {
 	}
 
 	public function index()
-	{	
+	{
 
 		$list = $this->getModuleList($module_code);
 
@@ -21,23 +21,33 @@ class ModulesController {
 	public function view($module_code)
 	{
 		$module = $this->getModule($module_code);
- 
- 		if($module_code !== $module_sits_code){
- 			Flight::redirect("module/".$module_sits_code);
- 		}
 
- 		return Flight::layout("modules/module", $module, "modules/layout");
+		if($module_code !== $module_sits_code){
+			Flight::redirect("module/".$module_sits_code);
+		}
+
+		return Flight::layout("modules/module", $module, "modules/layout");
 	}
 
-
-	public function legacy_url($module_code)
+	public function legacy_url($module_code = null)
 	{
+		// Redirect to module catalogue index page
+		if ($module_code === null) {
+			return Flight::redirect('/modules', 301);
+		}
+
+		// Redirect to collections... the 'splat' gets the wildcard (*) from the route
+		if ($page = @$module_code->splat) {
+			echo str_replace('.html', '', $page);
+			//TODO: update route once we have the search index
+			return Flight::redirect('/modules/?tab=' . $page, 301);
+		}
+
+		//redirect module pages
 		$module = $this->getModule($module_code);
- 		Flight::redirect("module/".$module->module_sits_code, 301);
- 	
+		Flight::redirect("module/".$module->module_sits_code, 301);
+
 	}
-
-
 
 	protected function getModule($code){
 		$data = Cache::load(KENT_API_URL ."v1/modules/module/".$code);
@@ -48,8 +58,4 @@ class ModulesController {
 		$data = Cache::load(KENT_API_URL ."v1/modules/collections/".$collection);
 		return json_decode($data['data']);
 	}
-
-
-
-
 }
