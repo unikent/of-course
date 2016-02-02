11dfdd341980f310f1a8d96b17bf12f5d4756039
thybag <thybag@gmail.com>
2016-02-02 15:34:35 +0000

    fix collection base_path

--- app/views/modules/collection.php
+++ app/views/modules/collection.php
@@ -24,6 +24,6 @@
 	<script type="text/javascript" charset="utf8" src="<?php echo Flight::asset('js/build/moduletable.min.js'); ?>"></script>
 	<script>
 	// Init first table
-	module_datatable($(".dataTable_main"), {"deferLoading":true, "api_endpoint": "<?php echo KENT_API_URL;?>v1/modules/collection/<?php echo  $collection; ?>"});
+	module_datatable($(".dataTable_main"), {"deferLoading":true, "api_endpoint": "<?php echo KENT_API_URL;?>v1/modules/collection/<?php echo  $collection; ?>", base_url: "<?php echo Flight::url('modules/module/'); ?>" });
  </script>
 </kentScripts>
