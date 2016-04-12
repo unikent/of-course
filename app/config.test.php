cd9943ae2aed9409a6fd6d9a51d2d2a4d3f19ed6
Edele Gormley <e.gormley@kent.ac.uk>
2016-04-12 11:05:04 +0200

    Update config.test.php

--- app/config.test.php
+++ app/config.test.php
@@ -7,6 +7,5 @@ const FAIL_ALERT_EMAIL = '';
 const LOCAL = false;
 const CLEARING = false;
 const CACHE_DIRECTORY = 'writable';
-const KENT_API_URL = '';
 const DISABLE_CURL_PROXY = false;
 const SHOW_UG_PREVIOUS_YEAR_BANNER = false;
