a9ebbc0d935c2b00fcc606bfe6fd969d67e77432
Edele Gormley <e.gormley@kent.ac.uk>
2016-04-12 11:04:41 +0200

    config wasn't up to date with server

--- app/config.test.php
+++ app/config.test.php
@@ -7,4 +7,6 @@ const FAIL_ALERT_EMAIL = '';
 const LOCAL = false;
 const CLEARING = false;
 const CACHE_DIRECTORY = 'writable';
+const KENT_API_URL = '';
+const DISABLE_CURL_PROXY = false;
 const SHOW_UG_PREVIOUS_YEAR_BANNER = false;
