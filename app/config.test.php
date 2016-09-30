dbef569e49a0df55611410fc3a9670e98158304c
Dom O'Connor <d.oconnor@kent.ac.uk>
2016-09-30 13:42:44 +0100

    Re add the test config

---
+++ app/config.test.php
@@ -0,0 +1,11 @@
+<?php
+const XCRI_WEBSERVICE = '';
+const KENT_API_URL = '';
+const TEMPLATING_ENGINE= '';
+const FAIL_ALERT_EMAIL = '';
+
+const LOCAL = false;
+const CLEARING = false;
+const CACHE_DIRECTORY = 'writable';
+const DISABLE_CURL_PROXY = false;
+const SHOW_UG_PREVIOUS_YEAR_BANNER = false;
