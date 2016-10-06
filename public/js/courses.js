76bb900961a3be74f728eec979596b12a4c7e777
Dom O'Connor <d.oconnor@kent.ac.uk>
2016-10-06 16:23:37 +0100

    Add build css

--- public/js/courses.js
+++ public/js/courses.js
@@ -37,108 +37,119 @@ $(document).ready(function(){
 		e.preventDefault();
 		$('#more').slideToggle();
 	});
+
+	$('.modal-nojs').hide();
+
 	/**
 	 * Apply page
 	 */
 
-	var $apply_form = $(".apply-form");
-	var $apply_link_ucas = $("#apply-link-ucas");
-	var $apply_link_courses = $(".apply-link-courses");
-	var $apply_link_dummy = $("#apply-link-dummy");
-	var $full_time_text = $(".full-time-text");
-	var $part_time_text =$(".part-time-text");
+	$('#applyButton').click(function() {
+
+		var $apply_form = $(".apply-form");
+		var $apply_link_ucas = $("#apply-link-ucas");
+		var $apply_link_courses = $(".apply-link-courses");
+		var $apply_link_dummy = $("#apply-link-dummy");
+		var $full_time_text = $(".full-time-text");
+		var $part_time_text =$(".part-time-text");
 
 
-	$apply_form.css('visibility', 'visible');
-	$apply_form.css('display', 'block');
+		$apply_form.css('visibility', 'visible');
+		$apply_form.css('display', 'block');
 
-	$apply_link_courses.hide();
-	$apply_link_dummy.show();
-	$apply_link_ucas.hide();
-	$full_time_text.hide();
-	$part_time_text.hide();
-
-	$apply_link_dummy.tooltip();
-
-	// the radio button list for the same-award-multiple-delivery edge case
-	$('input[type=radio][name=delivery]').change(function(){
 		$apply_link_courses.hide();
-		var id = 'apply-link-' + $(this).val();
-		$apply_link_dummy.hide();
-		$("#" + id).show();
-	});
+		$apply_link_dummy.show();
+		$apply_link_ucas.hide();
+		$full_time_text.hide();
+		$part_time_text.hide();
 
-	var $award = $("#award");
-	var $type = $("#type");
-	var $year = $("#year");
+		$apply_link_dummy.tooltip();
 
-	var award = $award.val();
-	var type = $type.val();
-	var year = $year.val();
-
-	updateApplyLinks();
-	changeType();
-
-	$award.change(function(){
-		award = $(this).val();
-		updateApplyLinks();
-	});
-	$type.change(function(){
-		type = $(this).val();
-		changeType();
-	});
-
-	function changeType() {
-
-		switch(type){
-			case 'full-time-ug-ucas':
-				$apply_link_ucas.show();
-				$apply_link_courses.hide();
-				$apply_link_dummy.hide();
-				$full_time_text.show();
-				$part_time_text.hide();
-				break;
-			case 'full-time-ug-direct':
-				$apply_link_ucas.hide();
-				type = 'full-time';
-				updateApplyLinks();
-				$apply_link_dummy.hide();
-				$full_time_text.show();
-				$part_time_text.hide();
-				break;
-			case 'full-time':
-				updateApplyLinks();
-				$full_time_text.show();
-				$part_time_text.hide();
-				break;
-			case 'part-time':
-				updateApplyLinks();
-				$apply_link_ucas.hide();
-				$full_time_text.hide();
-				$part_time_text.show();
-				break;
-			default :
-				// `pleaseselect` option
-				$apply_link_courses.hide();
-				$apply_link_ucas.hide();
-				$full_time_text.hide();
-				$part_time_text.hide();
-				$apply_link_dummy.show();
-				break;
-		}
-
-	}
-
-	function updateApplyLinks() {
-		linkid = 'apply-link-' + award + '-' + type + '-' + year;
-		$apply_link_courses.hide();
-		if ($("#" + linkid).length > 0) {
+		// the radio button list for the same-award-multiple-delivery edge case
+		$('input[type=radio][name=delivery]').change(function(){
+			$apply_link_courses.hide();
+			var id = 'apply-link-' + $(this).val();
 			$apply_link_dummy.hide();
-			$("#" + linkid).show();
+			$("#" + id).show();
+		});
+
+		var $award = $("#award");
+		var $type = $("#type");
+		var $year = $("#year");
+
+		var award = $award.val();
+		var type = $type.val();
+		var year = $year.val();
+
+		updateApplyLinks();
+		changeType();
+
+
+        $award.change(function(){
+			award = $(this).val();
+			updateApplyLinks();
+		});
+		$type.change(function(){
+			type = $(this).val();
+			changeType();
+		});
+
+		function changeType() {
+
+			switch(type){
+				case 'full-time-ug-ucas':
+					$apply_link_ucas.show();
+					$apply_link_courses.hide();
+					$apply_link_dummy.hide();
+					$full_time_text.show();
+					$part_time_text.hide();
+					break;
+				case 'full-time-ug-direct':
+					$apply_link_ucas.hide();
+					type = 'full-time';
+					updateApplyLinks();
+					$apply_link_dummy.hide();
+					$full_time_text.show();
+					$part_time_text.hide();
+					break;
+				case 'full-time':
+					updateApplyLinks();
+					$full_time_text.show();
+					$part_time_text.hide();
+					break;
+				case 'part-time':
+					updateApplyLinks();
+					$apply_link_ucas.hide();
+					$full_time_text.hide();
+					$part_time_text.show();
+					break;
+				default :
+					// `pleaseselect` option
+					$apply_link_courses.hide();
+					$apply_link_ucas.hide();
+					$full_time_text.hide();
+					$part_time_text.hide();
+					$apply_link_dummy.show();
+					break;
+			}
+
 		}
-		else {
-			$apply_link_dummy.show();
+
+		function updateApplyLinks() {
+			linkid = 'apply-link-' + award + '-' + type + '-' + year;
+			$apply_link_courses.hide();
+			if ($("#" + linkid).length > 0) {
+				$apply_link_dummy.hide();
+				$("#" + linkid).show();
+			}
+			else {
+				$apply_link_dummy.show();
+			}
 		}
-	}
+
+	});
+
+
+
 
 });
