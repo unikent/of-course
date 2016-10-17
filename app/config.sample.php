42d882d19764491099f02cf845654cc00eca0c20
thybag <thybag@gmail.com>
2016-10-17 13:38:50 +0100

    Add sample config

---
+++ app/config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+const XCRI_WEBSERVICE = 'https://webtools.kent.ac.uk/programmes/api/';
+const API_URL = 'https://api.kent.ac.uk/api';
+const THEME_LIBS = '/path/to/kentTheme';
+
+const FAIL_ALERT_EMAIL = '';
+const LOCAL = true;
+const DEBUG = true;
+const CLEARING = false;
+const CACHE_DIRECTORY = '/tmp';
