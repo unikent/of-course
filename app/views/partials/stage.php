bcc9e03d2d61e861ea7f2710aed9455f5c7cf736
gjmh-unikent <g.haddow@kent.ac.uk>
2016-11-16 10:00:58 +0000

    tweak module collapse behaviour and styles

--- app/views/partials/stage.php
+++ app/views/partials/stage.php
@@ -14,15 +14,17 @@
             <?php foreach ($cluster->modules->module as $module): ?>
                 <?php if ( ! empty($module->sds_code) && ! empty($module->module_title) && ! in_array($module->sds_code, $module_codes) ): ?>
                     <?php $module_codes[$module->sds_code] = $module->sds_code ?>
-                    <tr class="module-row" data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
-                        <td><span class="text-primary"><?php echo $module->sds_code ?> - <?php echo $module->module_title ?></span></td>
+                    <tr class="module-row collapsed" data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
+                        <td><span id="<?php echo $module->sds_code ?>" class="module-title"><?php echo $module->sds_code ?> - <?php echo $module->module_title ?></span></td>
 						<td class="text-xs-center"><?php echo $module->credit_amount; ?></td>
                         <td class="text-xs-center"><?php echo $module->ects_credit ?></td>
                     </tr>
-					<tr id="<?php echo $module->sds_code; ?>-more" class="collapse" data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
+					<tr id="<?php echo $module->sds_code; ?>-more" class="collapse">
 						<td class="more" colspan="3">
-						<p><?php echo preg_replace("/\n/",'</p><p>',preg_replace('/[\r\n]+/', "\n", preg_replace('/<br\s*\/?>/',"\n",$module->synopsis))); ?></p>
-						<a class="chevron-link" href="http://www.kent.ac.uk/courses/modules/module/<?php echo $module->sds_code ?>">Read more</a>
+							<div data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
+								<p><?php echo preg_replace("/\n/",'</p><p>',preg_replace('/[\r\n]+/', "\n", preg_replace('/<br\s*\/?>/',"\n",$module->synopsis))); ?></p>
+							</div>
+							<a aria-labelledby="#<?php echo $module->sds_code ?>" class="chevron-link d-block" href="/courses/modules/module/<?php echo $module->sds_code ?>">Read more</a>
 						</td>
 					</tr>
                 <?php endif; ?>
@@ -34,15 +36,17 @@
             <?php foreach ($cluster->modules->module as $module): ?>
                 <?php if ( ! empty($module->sds_code) && ! in_array($module->sds_code, $module_codes) ): ?>
                     <?php $module_codes[$module->sds_code] = $module->sds_code ?>
-					<tr class="module-row" data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
-						<td><span class="text-primary"><?php echo $module->sds_code ?> - <?php echo $module->module_title ?></span></td>
+					<tr class="module-row collapsed" data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
+						<td><span id="<?php echo $module->sds_code ?>" class="module-title"><?php echo $module->sds_code ?> - <?php echo $module->module_title ?></span></td>
 						<td class="text-xs-center"><?php echo $module->credit_amount; ?></td>
 						<td class="text-xs-center"><?php echo $module->ects_credit ?></td>
 					</tr>
-					<tr id="<?php echo $module->sds_code; ?>-more" class="collapse" data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
-						<td class="more" colspan="3">
-							<p><?php echo preg_replace("/\n/",'</p><p>',preg_replace('/[\r\n]+/', "\n", preg_replace('/<br\s*\/?>/',"\n",$module->synopsis))); ?></p>
-							<a class="chevron-link" href="http://www.kent.ac.uk/courses/modules/module/<?php echo $module->sds_code ?>">Read more</a>
+					<tr id="<?php echo $module->sds_code; ?>-more" class="collapse">
+						<td class="more" colspan="3" >
+							<div data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
+								<p><?php echo preg_replace("/\n/",'</p><p>',preg_replace('/[\r\n]+/', "\n", preg_replace('/<br\s*\/?>/',"\n",$module->synopsis))); ?></p>
+							</div>
+							<a aria-labelledby="#<?php echo $module->sds_code ?>" class="chevron-link d-block" href="/courses/modules/module/<?php echo $module->sds_code ?>">Read more</a>
 						</td>
 					</tr>
                 <?php endif; ?>
--- app/views/pg/tabs/structure.php
+++ app/views/pg/tabs/structure.php
@@ -36,15 +36,17 @@ foreach ($course->modules as $module) {
 		<?php
 		$course->module_list = empty($course->module_list)?array():$course->module_list;
 		foreach ($course->module_list as $module): ?>
-			<tr class="module-row" data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
-				<td><span class="text-primary"><?php echo $module->sds_code ?> - <?php echo $module->module_title ?></span></td>
+			<tr class="module-row collapsed" data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
+				<td><span id="<?php echo $module->sds_code ?>" class="module-title"><?php echo $module->sds_code ?> - <?php echo $module->module_title ?></span></td>
 				<td class="text-xs-center"><?php echo $module->credit_amount; ?></td>
 				<td class="text-xs-center"><?php echo $module->ects_credit ?></td>
 			</tr>
-			<tr id="<?php echo $module->sds_code; ?>-more" class="collapse" data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
+			<tr id="<?php echo $module->sds_code; ?>-more" class="collapse">
 				<td class="more" colspan="3">
-					<p><?php echo preg_replace("/\n/",'</p><p>',preg_replace('/[\r\n]+/', "\n", preg_replace('/<br\s*\/?>/',"\n",$module->synopsis))); ?></p>
-					<a class="chevron-link" href="http://www.kent.ac.uk/courses/modules/module/<?php echo $module->sds_code ?>">Read more</a>
+					<div data-toggle="collapse" data-target="#<?php echo $module->sds_code; ?>-more">
+						<p><?php echo preg_replace("/\n/",'</p><p>',preg_replace('/[\r\n]+/', "\n", preg_replace('/<br\s*\/?>/',"\n",$module->synopsis))); ?></p>
+					</div>
+					<a aria-labelledby="#<?php echo $module->sds_code ?>" class="chevron-link d-block" href="/courses/modules/module/<?php echo $module->sds_code ?>">Read more</a>
 				</td>
 			</tr>
 	<?php endforeach; ?>
