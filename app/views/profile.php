bee3fa42462c89d62d87ca066df297c9d53c6540
gjmh-unikent <g.haddow@kent.ac.uk>
2016-11-29 10:39:16 +0000

    add video launcher capability to profile page header feature

--- app/views/profile.php
+++ app/views/profile.php
@@ -4,10 +4,26 @@ use \unikent\kent_theme\kentThemeHelper;
 $banner_image = (empty($profile->banner_image_id)) ? Flight::asset('images/default-profile-feature.jpg') : $profile->banner_image_id->url;
 $banner_image_alt = (empty($profile->banner_image_id)) ?'Students preparing for their graduation ceremony at Canterbury Cathedral' : $profile->banner_image_id->caption;
 
+
+if(!empty($profile->video)){
+	$matches = false;
+	preg_match('/^https?:\/\/(?:youtu\.be|(?:www\.|m\.)?youtube\.com)\/?(?:watch|embed)?(?:.*?v=|v\/|\/)([\w-]+)/i',$profile->video,$matches);
+	if(count($matches) !== 2){
+		$profile->video = false;
+	}else{
+		$profile->video = $matches[1];
+	}
+}
+
 ?>
 <div class="card card-overlay header-card-overlay  ">
 	<div class="card-body">
-		<div class="card-media-wrap">
+		<div class="card-media-wrap<?php echo $profile->video?' video-launcher':''; ?>'"<?php echo $profile->video?' data-mode="fullscreen"':''; ?>>
+			<?php if($profile->video){ ?>
+			<div class="video-player">
+				<div data-video-id="<?php echo $profile->video; ?>" data-type="youtube">&nbsp;</div>
+			</div>
+			<?php } ?>
 			<img class="card-img" src="<?php echo $banner_image; ?>" alt="<?php echo $banner_image_alt; ?>">
 			<div class="attribution">
 				<i class="kf-camera"></i>
@@ -15,17 +31,11 @@ $banner_image_alt = (empty($profile->banner_image_id)) ?'Students preparing for 
 			</div>
 		</div>
 		<div class="card-title-wrap force-block content-contained pull-bottom ">
-
-
 			<h1 class="card-title">Student Profile
 				<div class="text-white"><?php echo $profile->name; ?></div>
 			</h1>
-
-
 			<p class="card-text"><?php echo $profile->course; ?></p>
 		</div>
-
-
 	</div>
 </div>
 <div class="content-container">
