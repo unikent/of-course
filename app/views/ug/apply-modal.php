e0d557ac83da1beb0012e1425fe81ef2701231bb
Dom O'Connor <D.OConnor@kent.ac.uk>
2016-12-08 15:36:31 +0000

    Lowercase Now

--- app/views/ug/apply-modal.php
+++ app/views/ug/apply-modal.php
@@ -30,7 +30,7 @@ if (empty($course->deliveries)) {
 				<div class="container px-2">
 					<div class="row">
 						<div class="col-md-10">
-					<h2 class="modal-title">Apply Now</h2>
+					<h2 class="modal-title">Apply now</h2>
 						<?php
 							if (!$has_parttime){
 								?>
@@ -65,7 +65,7 @@ if (empty($course->deliveries)) {
 					</div>
 				</div>
 			</div>
-            <div class="modal-body container px-2 py-1">
+            <div class="modal-body container px-2 py-2">
 				<div class="row">
 					<div class="col-lg-8">
 						<?php if ($course->how_to_apply_atypical_courses != ''){
