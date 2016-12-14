c79620f965fe365815aa5507e91dfc2193d21667
gjmh-unikent <g.haddow@kent.ac.uk>
2016-12-14 15:01:09 +0000

    move pg fees and funding link to bottom

--- app/views/pg/top-sidebar.php
+++ app/views/pg/top-sidebar.php
@@ -6,9 +6,6 @@
     <?php if (!empty($course->key_information_miscellaneous)): ?>
         <li class="nav-item"><a href="#study-support" data-toggle="tab" role="tab" class="nav-link">Study support</a></li>
     <?php endif; ?>
-	<?php if (!(isset($course->no_fee_output) && $course->no_fee_output === 'true')){ ?>
-		<li class="nav-item"><a href="#fees" data-toggle="tab" role="tab" class="nav-link">Fees and funding</a></li>
-	<?php } ?>
     <?php if (!empty($course->careers_and_employability) || !empty($course->globals->careersemployability_text) || !empty($course->professional_recognition)): ?>
         <li class="nav-item"><a href="#careers" data-toggle="tab" role="tab" class="nav-link">Careers</a></li>
     <?php endif; ?>
@@ -17,4 +14,7 @@
         <li class="nav-item"><a href="#research-areas" data-toggle="tab" role="tab" class="nav-link">Research areas</a></li>
     <?php endif; ?>
     <li class="nav-item"><a href="#staff-research" data-toggle="tab" role="tab" class="nav-link">Staff research</a></li>
+    <?php if (!(isset($course->no_fee_output) && $course->no_fee_output === 'true')){ ?>
+        <li class="nav-item"><a href="#fees" data-toggle="tab" role="tab" class="nav-link">Fees and funding</a></li>
+	<?php } ?>
 </ul>
