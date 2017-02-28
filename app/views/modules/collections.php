f3f1734c5cf0a4cbab68330a7cd68ec5e8896464
Dom O'Connor <D.OConnor@kent.ac.uk>
2017-02-28 15:18:57 +0000

    Fix module collection

--- app/views/modules/collections.php
+++ app/views/modules/collections.php
@@ -1,9 +1,12 @@
-<h1>Module Catalogue</h1>
+<div class="container">
+	<?php \unikent\kent_theme\kentThemeHelper::breadcrumb(array("Courses"=>"/courses/", "Modules"=>"/courses/modules", "Collections"=>"")); ?>
+	<h1>Module Catalogue</h1>
 
-<h2>Collections</h2>
+	<h2>Collections</h2>
 
-<ul>
-	<?php foreach($collections as $code => $collection){ ?>
-			<li><a href="<?php echo Flight::url("modules/collection/".$collection->code); ?>"><?php echo $collection->title ?></a></li>	
-	<?php } ?>	
-</ul>
+	<ul>
+		<?php foreach($collections as $code => $collection){ ?>
+			<li><a href="<?php echo Flight::url("modules/collection/".$collection->code); ?>"><?php echo $collection->title ?></a></li>
+		<?php } ?>
+	</ul>
+</div>
