c3a8747eec5259af8a7cf14b7ec0522768f62dfd
Matthew Bull <m.bull@kent.ac.uk>
2017-04-05 15:07:33 +0100

    created a universal filter across courses, modules, and profiles
    Course, module and profile filtering now looks and behaves the same
    across each

--- app/views/modules/collection.php
+++ app/views/modules/collection.php
@@ -6,23 +6,11 @@
 	<a href='<?php echo Flight::url("modules/collection");?>' class="pull-right">Browse all collections &raquo;</a>
 	<h2><?php echo $modules->title;?> </h2>
 </div>
-<div class="panel-secondary">
-		<div class="container form-inline pt-2 pb-2 filter-box" id="filter_box">
-			<div class="module-filter-container">
-				<input
-						id="module-filter"
-						class="form-control"
-						type="text"
-						placeholder="Search modules"
-						data-quickspot-config="modules_inline"
-						data-quickspot-source="<?php echo API_URL; ?>/v1/modules/collection/<?php echo $collection?>"
-						data-quickspot-target="quickspot-output"
-						data-quickspot-filters="filter_box"
-				>
-			</div>
+<div class="filter-header panel-secondary">
+		<div class="filter-categories container form-inline" id="filter_categories">
 
-			<div class="search-select module-options-search-div">
-				<select class="subject-search form-control "  data-filter-col="sds_code">
+			<div class="filter-select">
+				<select class="custom-select subject-search form-control" data-filter-col="sds_code">
 					<option value="">All subjects</option>
 					<?php foreach ($subjects as $k => $v){ ?>
 						<option value="<?php echo $k; ?>"><?php echo $v; ?> - (<?php echo $k; ?>)</option>
@@ -33,8 +21,26 @@
 			<input type="hidden" name="quickspot_result_count" />
 			<input type="hidden" name="qucikspot_return_to_scroll_position" />
 		</div>
+
+		<div class="filter-text container form-inline">
+			<h2><span id="filter_title">All</span> modules</h2>
+			<div class="filter-container">
+				<input
+						id="module-filter"
+						class="form-control"
+						type="text"
+						placeholder="Filter module list by keyword"
+						data-quickspot-config="modules_inline"
+						data-quickspot-source="<?php echo API_URL; ?>/v1/modules/collection/<?php echo $collection?>"
+						data-quickspot-target="quickspot-output"
+						data-quickspot-filters="filter_categories"
+						data-quickspot-filter-text-target="filter_title"
+				>
+			</div>
+		</div>
 	</div>
-	<div class="card-panel cards-list cards-backed card-panel-secondary module-listing">
-		<div class="card-panel-body quickspot-output" id="quickspot-output">
+
+	<div class="filter-results card-panel cards-list cards-backed card-panel-secondary">
+		<div class="card-panel-body standard-output" id="quickspot-output">
 		</div>
 	</div>
