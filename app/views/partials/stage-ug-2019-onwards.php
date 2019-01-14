5e5ebeb8044cf007cd233533bfa648341a139fdd
Christian Cable <c.f.cable@kent.ac.uk>
2019-01-14 14:12:43 +0000

    WIP - ug module display

---
+++ app/views/partials/stage-ug-2019-onwards.php
@@ -0,0 +1,101 @@
+<h2>@TODO 2019 Onwards</h2>
+
+<?php if ( ! empty($stage->clusters->compulsory) || ! empty($stage->clusters->optional) ): ?>
+
+<?php
+
+	$module_clusters = array();
+	if (!empty($stage->clusters->compulsory)) {
+		$module_clusters = array_merge($module_clusters, $stage->clusters->compulsory);
+	}
+	if (!empty($stage->clusters->optional)) {
+		$module_clusters = array_merge($module_clusters, $stage->clusters->optional);
+	}
+
+	$optional_clusters = array();
+	$required_clusters = array();
+
+	foreach ($module_clusters as $cluster) {
+		if (cluster_contains_opional_modules($cluster)) {
+			$optional_clusters[]= $cluster;
+		} else {		
+			$required_clusters[]= $cluster;
+		}
+	}
+?>
+
+<?php $module_codes = array(); ?>
+
+<p>TODO: Optional free text here</p>
+
+
+	<?php if (!empty($required_clusters)): ?>
+
+		<table class="table">
+		<thead>
+		<tr>
+			<th width="70%">Compulsory modules currently include</th>
+			<th class="text-xs-center">Credits</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php foreach ($required_clusters as $cluster): ?>
+			<?php foreach ($cluster->modules->module as $module): ?>
+				<?php if ( ! empty($module->sds_code) && ! empty($module->module_title) && ! in_array($module->sds_code, $module_codes) ): ?>
+					<?php $module_codes[$module->sds_code] = $module->sds_code ?>
+					<tr class="module-row">
+						<td class="module-text">
+							<span data-toggle="collapse" data-target="#<?php echo $stage_id . '-' . $module->sds_code; ?>-more" id="<?php echo $module->sds_code ?>" class="module-row collapsed module-title"><?php echo $module->sds_code ?> - <?php echo $module->module_title ?></span>
+							<div class="collapse" id="<?php echo $stage_id . '-' . $module->sds_code; ?>-more">
+								<div class="more">
+									<p><?php echo preg_replace("/\n/",'</p><p>',preg_replace('/[\r\n]+/', "\n", preg_replace('/<br\s*\/?>/',"\n",$module->synopsis))); ?></p>
+									<a aria-labelledby="#<?php echo $module->sds_code ?>" class="chevron-link" href="/courses/modules/module/<?php echo $module->sds_code ?>">Read more</a>
+								</div>
+							</div>
+						</td>
+						<td class="text-xs-center"><?php echo $module->credit_amount; ?></td>
+					</tr>
+				<?php endif; ?>
+			<?php endforeach; ?>
+		<?php endforeach; ?>
+	<?php endif; ?>
+
+
+	<?php if (!empty($optional_clusters)): ?>
+
+		<table class="table">
+		<thead>
+		<tr>
+			<th width="70%">Optional modules may include</th>
+			<th class="text-xs-center">Credits</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php foreach ($optional_clusters as $cluster): ?>
+			<?php foreach ($cluster->modules->module as $module): ?>
+				<?php if ( ! empty($module->sds_code) && ! empty($module->module_title) && ! in_array($module->sds_code, $module_codes) ): ?>
+					<?php $module_codes[$module->sds_code] = $module->sds_code ?>
+					<tr class="module-row">
+						<td class="module-text">
+							<span data-toggle="collapse" data-target="#<?php echo $stage_id . '-' . $module->sds_code; ?>-more" id="<?php echo $module->sds_code ?>" class="module-row collapsed module-title"><?php echo $module->sds_code ?> - <?php echo $module->module_title ?></span>
+							<div class="collapse" id="<?php echo $stage_id . '-' . $module->sds_code; ?>-more">
+								<div class="more">
+									<p><?php echo preg_replace("/\n/",'</p><p>',preg_replace('/[\r\n]+/', "\n", preg_replace('/<br\s*\/?>/',"\n",$module->synopsis))); ?></p>
+									<a aria-labelledby="#<?php echo $module->sds_code ?>" class="chevron-link" href="/courses/modules/module/<?php echo $module->sds_code ?>">Read more</a>
+								</div>
+							</div>
+						</td>
+						<td class="text-xs-center"><?php echo $module->credit_amount; ?></td>
+					</tr>
+				<?php endif; ?>
+			<?php endforeach; ?>
+		<?php endforeach; ?>
+	<?php endif; ?>
+
+
+<?php endif; ?>
+</tbody>
+</table>
+<?php if ( ! empty($stage->clusters->wildcard) ): ?>
+	<em>You have the opportunity to select elective modules in this stage</em>
+<?php endif; ?>
