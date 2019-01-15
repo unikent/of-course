ae163b920d6ac581232a21e6d972474d5c06ce12
Sam Yapp <sam@samyapp.com>
2019-01-15 08:12:00 +0000

    Dont display non-existant reading list urls

--- app/views/modules/tabs/reading.php
+++ app/views/modules/tabs/reading.php
@@ -5,6 +5,8 @@
 
 						<?php if(isset($module->reading_lists)): ?>
 							<?php foreach ($module->reading_lists as $campus => $url): ?>
-								<p><a href="<?php echo $url ?>">See the library reading list for this module (<?php echo ucfirst($campus); ?>)</a></p>
+								<?php if($url):?>
+									<p><a href="<?php echo $url ?>">See the library reading list for this module (<?php echo ucfirst($campus); ?>)</a></p>
+								<?php endif;?>
 							<?php endforeach; ?>
 						<?php endif; ?>
