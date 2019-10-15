1cc451baba2d43f658bc554260257e1c3b12cceb
Christian Cable <c.f.cable@kent.ac.uk>
2019-10-15 11:57:11 +0100

    dummy open days banner code for ux to make useful and beautiful

--- app/views/partials/notices.php
+++ app/views/partials/notices.php
@@ -78,3 +78,19 @@
 		This is a <?php echo $course->year;?> entry programme. Would you like to <a href='<?php echo $previousYear; ?>'> view <?php echo $course->programme_title;?> for <?php echo $course->current_year-1;?> entry?</a>
 	</div>
 <?php endif;?>
+
+<?php if (isset($course) && ("true" == $course->globals->open_day_banner_display_banner)) : ?>
+	<div class="card card-backed-tertiary">
+		<div class="card-block">
+			<h2><?php echo $course->globals->open_day_banner_heading?></h2>
+			
+			<div><?php echo $course->globals->open_day_banner_text ?></div>
+			
+			<a href="<?php echo $course->globals->open_day_banner_target_url?>">
+				<button type="button" class="btn btn-primary">
+					<?php echo $course->globals->open_day_banner_call_to_action_button_text?>
+				</button>
+			</a>
+		</div>
+	</div>
+<?php endif; ?>
